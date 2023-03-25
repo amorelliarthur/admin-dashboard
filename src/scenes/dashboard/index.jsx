ef6af36b2a270a5d92498f5a503ce577d8f72522
Arthur Amorelli <arthur-soares@hotmail.com>
2023-03-25 15:47:32 -0300

    Dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,13 @@
+import Header from "../../components/Header"
+import { Box } from "@mui/material"
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <Box m="20px">
+      <Box display="flex" jusfifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
   )
 }
 
