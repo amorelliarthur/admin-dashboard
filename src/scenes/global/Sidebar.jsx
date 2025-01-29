6d23537e8869ebe22ec37144b62219b504f0eae7
Arthur Amorelli <arthur-soares@hotmail.com>
2025-01-29 20:56:18 -0300

    ajustes

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,7 +107,7 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Fleurys Gilmore
+                  Arthur
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   Admin
